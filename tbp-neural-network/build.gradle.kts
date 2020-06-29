8367169f2c90a2ac6e5b6447a349f48b8909a2a5
Stan van der Bend <stanvdbend@gmail.com>
2020-06-29 21:27:26 +0200

    Added deeplearning4j and ported example from https://deeplearning4j.konduit.ai/getting-started/tutorials/recurrent-networks

--- tbp-neural-network/build.gradle.kts
+++ tbp-neural-network/build.gradle.kts
@@ -1,3 +1,5 @@
 dependencies {
     implementation("com.github.haifengl:smile-core:2.4.0")
+    implementation("org.deeplearning4j:deeplearning4j-core:1.0.0-beta7")
+    implementation("org.nd4j:nd4j-native-platform:1.0.0-beta7")
 }
